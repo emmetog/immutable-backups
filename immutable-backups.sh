d57bf755a800b14b8f21f382c6f99dc8c066cdfb
Emmet O'Grady <emmet789@gmail.com>
2023-06-27 17:43:04 +0100

    Improvements to incremental restore

--- immutable-backups.sh
+++ immutable-backups.sh
@@ -52,7 +52,7 @@ function showHelpBackup {
 
 function showHelpRestore {
     echo ""
-    echo "Usage: $(basename $0) restore --source=[source] --remote=[remote] (--date=[date]) (--incremental) (--dry-run)"
+    echo "Usage: $(basename $0) restore --source=[source] --remote=[remote] (--date=[date]) (--incremental) (--incremental-date-start=[date]) (--dry-run)"
     echo ""
     echo "    where:"
     echo ""
@@ -61,20 +61,24 @@ function showHelpRestore {
     echo ""
     echo "    optional:"
     echo ""
-    echo "         --date             A date in the format Y-M-d-Hms to restore, default is to use the latest backup date found"
-    echo "         --incremental      Should be specified if the backup being restored was incremental"
-    echo "         --dry-run          If specified, dont copy anything, just test"
-    echo "         --verbose          If specified, output is more verbose"
+    echo "         --date                    A date in the format Y-M-d-Hms to restore until, default is to use the latest backup date found"
+    echo "         --incremental             Should be specified if the backup being restored was incremental"
+    echo "         --incremental-date-start  A date in the format Y-M-d-Hms to restore incremental backups after, default is to restore full history until --date (implies --incremental)"
+    echo "         --dry-run                 If specified, dont copy anything, just test"
+    echo "         --verbose                 If specified, output is more verbose"
     echo ""
     echo "Examples:"
     echo "    # Restore the latest version of an incremental backup"
     echo "    $(basename $0) restore --remote=your-remote: --local=/path/to/put/restored --incremental --dry-run"
     echo ""
+    echo "    # Restore a specific point in time incremental backup"
+    echo "    $(basename $0) restore --remote=your-remote: --local=/path/to/put/restored --date=2021-04-21-183015 --incremental --dry-run"
+    echo ""
+    echo "    # Restore only changes between two dates, incremental backup"
+    echo "    $(basename $0) restore --remote=your-remote: --local=/path/to/put/restored --date=2021-04-21-183015 --incremental-date-start=2021-04-22-041735 --dry-run"
+    echo ""
     echo "    # Restore the latest version of a full backup"
     echo "    $(basename $0) restore --remote=your-remote: --local=/path/to/put/restored --dry-run"
-    echo ""
-    echo "    # Restore a specific point in time incremental backup"
-    echo "    $(basename $0) restore --remote=your-remote: --local=/path/to/put/restored --date=2021-04-21-183015 --dry-run"
     echo ""
     echo "Notes:"
     echo "  * This requires rclone to be installed and the remotes to be configured on this system. See the rclone"
@@ -93,6 +97,7 @@ incremental=false
 local=""
 remote=""
 date=""
+incrementalDateStart=""
 dry_run_flag=""
 verbose_flag=""
 RCLONE_BIN=${RCLONE_BIN:-/usr/bin/rclone}
@@ -169,12 +174,12 @@ if [ "$operation" == "backup" ]; then
             compareDestFlags[i]=" --compare-dest=${remote}/incremental/${path}"
         done
 
-        $RCLONE_BIN copy --no-traverse --immutable ${local} ${remote}/incremental/$(date +"%Y-%m-%d-%H%M%S") ${dry_run_flag} ${compareDestFlags[*]} ${verbose_flag}
+        $RCLONE_BIN copy --no-traverse --immutable --copy-links ${local} ${remote}/incremental/$(date +"%Y-%m-%d-%H%M%S") ${dry_run_flag} ${compareDestFlags[*]} ${verbose_flag}
 
     else
          echo "$(date) Starting full backup"
 
-        $RCLONE_BIN copy --no-traverse --immutable ${local} ${remote}/full/$(date +"%Y-%m-%d-%H%M%S") ${dry_run_flag} ${verbose_flag}
+        $RCLONE_BIN copy --no-traverse --immutable --copy-links ${local} ${remote}/full/$(date +"%Y-%m-%d-%H%M%S") ${dry_run_flag} ${verbose_flag}
     fi
 elif [ "$operation" == "restore" ]; then
 
@@ -182,6 +187,13 @@ elif [ "$operation" == "restore" ]; then
             case "$1" in
                     -h|--help)
                             showHelpRestore
+                            ;;
+                    --incremental-date-start*)
+                            incrementalDateStart=$(echo $1 | sed -e 's/^[^=]*=//g')
+                            incremental=true
+                            export incrementalDateStart
+                            export incremental
+                            shift
                             ;;
                     --incremental*)
                             incremental=true
@@ -258,18 +270,32 @@ elif [ "$operation" == "restore" ]; then
             for prevBackup in "${prevBackups[@]}"; do
 
                 if [[ "$date" > "$prevBackup" ]]; then
-                    datesToRestore+=($prevBackup)
+
+                    if [[ " $incrementalDateStart " == "  " ]]; then
+                        echo "adding $prevBackup because backup is before target date $date (and incrementalDateStart is not set)"
+                        datesToRestore+=($prevBackup)
+                    fi
+
+                    if [[ "$incrementalDateStart" < "$prevBackup" ]]; then
+                        echo "adding $prevBackup because it's after incrementalDateStart: $incrementalDateStart"
+                        datesToRestore+=($prevBackup)
+                    fi
+
+                    if [[ "$incrementalDateStart" == "$prevBackup" ]]; then
+                        echo "adding $prevBackup because exact match incrementalDateStart: $incrementalDateStart"
+                        datesToRestore+=($prevBackup)
+                    fi
                 fi
 
                 if [ " $date " == " $prevBackup " ]; then
+                    echo "adding $prevBackup because exact match date: $date"
                     datesToRestore+=($prevBackup)
-                    dateFound=true
-                    break
                 fi
             done
 
-            if [ $dateFound != true ]; then
-                echo "Date '$date' not found in the remote, specify a valid backup date or don't specify --date to restore the most recent backup"
+            if [ "${#datesToRestore[@]}" -lt 1 ]; then
+                echo "No dates matched in the remote, review your --incremental-date-start and --date arguments and see the remote for \
+                dates there"
                 echo "The most recent 10 backups listed newest first are (check the remote to see them all):"
 
                 mostRecentBackupDates=("${prevBackupsNewestFirst[@]:0:10}")
@@ -282,7 +308,16 @@ elif [ "$operation" == "restore" ]; then
             fi
         fi
 
+        datesToRestore=($(for date in "${datesToRestore[@]}"; do echo "${date}"; done | sort --unique))
         cnt=${#datesToRestore[@]}
+
+        echo "============"
+        echo "Summary of versions to restore:"
+        for date in "${datesToRestore[@]}"; do
+            echo "  $date"
+        done
+
+        echo "============"
 
         # Now loop over all backups and apply them incrementally, starting with the oldest until
         # we reach the desired date
@@ -296,7 +331,7 @@ elif [ "$operation" == "restore" ]; then
 
             echo "$(date) processing backup: $prevBackup $counter/$cnt ($percentComplete%)"
 
-            $RCLONE_BIN copy --no-traverse ${remote}/incremental/${prevBackup} ${local} ${dry_run_flag} ${verbose_flag}
+            $RCLONE_BIN copy --no-traverse --copy-links ${remote}/incremental/${prevBackup} ${local} ${dry_run_flag} ${verbose_flag}
         done
 
     else
@@ -337,7 +372,7 @@ elif [ "$operation" == "restore" ]; then
             remote="$remote/full/${prevBackups[0]}"
         fi
 
-        $RCLONE_BIN copy --no-traverse --immutable ${remote} ${local} ${dry_run_flag} ${verbose_flag}
+        $RCLONE_BIN copy --no-traverse --immutable --copy-links ${remote} ${local} ${dry_run_flag} ${verbose_flag}
     fi
 else
     # Invalid operation
